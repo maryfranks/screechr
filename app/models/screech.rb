affafece1d2b4aaa70235f6a7d5135f6a9c17f17
maryfranks <maryfranks@gmail.com>
2017-11-29 09:39:13 -0500

    screech and user model validations

--- app/models/screech.rb
+++ app/models/screech.rb
@@ -1,2 +1,5 @@
 class Screech < ApplicationRecord
+  belongs_to :user
+
+  validates :content, :length => { :maximum => 1024 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :screeches
+
+  validates :username, :length => { :maximum => 80 }, :allow_blank => false, :uniqueness => true, presence: true
+  validates :first_name, :last_name, :length => { :maximum => 100 }, :allow_blank => false, presence: true
 end
