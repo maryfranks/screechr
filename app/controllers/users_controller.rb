83e234389c33dc4af09e04e429a5cbba9842f57f
maryfranks <maryfranks@gmail.com>
2017-11-30 09:00:13 -0500

    user can log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      flash.now[:notice] = 'Account successfully created!'
+      redirect_to user_path
+    else
+      flash.now[:error] = 'Sorry, try again!'
+      render :new
+    end
   end
 
   def update
@@ -16,8 +23,14 @@ class UsersController < ApplicationController
 
   end
 
-  def show 
+  def show
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :password, :password_confirmation)
   end
 
 end
