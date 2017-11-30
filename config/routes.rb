645fbeb256f894274cc55a24eb45ddda05400bfe
maryfranks <maryfranks@gmail.com>
2017-11-30 11:47:37 -0500

    add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :screeches, defaults: {format: :json}
   resource :user, defaults: {format: :json}
+  resources :sessions, only: [:new, :create, :destroy]
 end
