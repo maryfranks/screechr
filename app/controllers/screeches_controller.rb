36075c0dcf7186cbde25eaa34200f7690219cd7a
maryfranks <maryfranks@gmail.com>
2017-11-30 08:06:58 -0500

    change default response type to json

--- app/controllers/screeches_controller.rb
+++ app/controllers/screeches_controller.rb
@@ -1,5 +1,18 @@
 class ScreechesController < ApplicationController
 
+  def index
+    @screeches = Screech.all
+
+    respond_to do | format |
+
+      format.json do
+        render json: @screeches
+      end
+      
+    end
+
+  end
+
   def show
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :screeches
-  resource :user
+  resources :screeches, defaults: {format: :json}
+  resource :user, defaults: {format: :json}
 end
